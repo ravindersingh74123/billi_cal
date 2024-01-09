c7bf4aad90249e681ba3017337c4aa2dc94263b3
ravindersingh74123 <154728708+ravindersingh74123@users.noreply.github.com>
2024-01-09 17:40:50 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -10,7 +10,38 @@ function toggleImageTransition() {
     audio.play();
     
 }
+function displayPhoto() {
+    const photoContainer = document.getElementById("huh");
+    
+    // Clear previous content
+    photoContainer.innerHTML = '';
 
+    // Display a photo
+    const photo = document.createElement('img');
+    photo.src = 'huh_cat.jpg'; // Replace with your photo URL
+    photo.alt = 'Photo';
+    photo.className = 'w-64 h-auto';
+    photoContainer.appendChild(photo);
+}
+
+function displayVideo() {
+    const videoContainer = document.getElementById("koyl");
+    
+    // Clear previous content
+    // photoContainer.innerHTML = '';
+    videoContainer.innerHTML = '';
+
+    // Display a video
+    const video = document.createElement('video');
+    video.className = 'w-64 h-auto';
+    video.controls = true;
+    const source = document.createElement('source');
+    source.src = 'koyl.mp4'; // Replace with your video URL
+    source.type = 'video/mp4';
+    video.appendChild(source);
+    videoContainer.appendChild(video);
+    video.play();
+}
 
 function sum(opcode){
     if(opcode=="sum"){
@@ -20,7 +51,14 @@ function sum(opcode){
         
     
         let suma= value1+value2;
-        third.value=suma;
+        if(suma ==7){
+            displayVideo();
+            third.value=suma;
+        }
+        else{
+            third.value=suma;
+        }
+       
     }
     else if(opcode=="mul"){
         let value1=parseInt(first.value);
@@ -40,7 +78,9 @@ function sum(opcode){
         let value1=parseInt(first.value);
         let value2=parseInt(second.value);  
         if( value2 ==0){
-            toggleImageTransition();
+            // toggleImageTransition();
+            displayPhoto();
+            
         }
         else{
             let suma= value1/value2;
